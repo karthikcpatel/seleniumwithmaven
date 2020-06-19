a5eaf205da7f3b5170b8ff109ce158f434b08721
karthikcpatel <karthikcpatel@gmail.com>
2020-06-19 14:56:10 +0530

    initial commit

---
+++ SeleniumWithMaven/src/test/java/seleniumwithmaven/Login.java
@@ -0,0 +1,12 @@
+package seleniumwithmaven;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class Login{
+	WebDriver driver;
+	@Test
+	public void seleniumwithmaven(){
+	System.out.println("Welcome to the world of Maven!");
+}
+}
